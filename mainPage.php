5cea9b076199b6a8533cc352bb0e119ced5b4af0
vivianz1023 <86883079+vivianz1023@users.noreply.github.com>
2022-04-02 08:50:18 -0700

    Update mainPage.php
    change php files' name

--- mainPage.php
+++ mainPage.php
@@ -1,60 +1,50 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>MainPage Information</title>
-        <style>
-            body {
-                background-image: url("mainBG.png");
-                background-repeat: no-repeat, repeat;
-                margin: 30px 30px;
-            }
-            p {font-family:Gill Sans; font-size:20px;}
-        </style>
-    </head>
+<head>
+    <title>MainPage Information</title>
+    <style>
+        body {
+            background-image: url("mainBG.png");
+            background-repeat: no-repeat, repeat;
+            margin: 30px 30px;
+        }
+        p {font-family:Gill Sans; font-size:20px;}
+    </style>
+</head>
+<body>
+<b style="font-family:cursive; font-size:60px">Olympic Information Menu Page</b>
+<br /><br /><br />
+<b style="font-family:Gill Sans; font-size:20px;">Hi, visitor! Press one of the following buttons to looking for the Olympic information you wanted!</b>
+<br /><br />
 
+<form method="GET" action="https://www.students.cs.ubc.ca/~xnie0204/VolunteerPage.php">
+    <p>Learn about the information of the Volunteer, please press:&nbsp;
+        <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Volunteer" name="Volunteer"></p>
+</form>
 
-    <body>
-        <b style="font-family:cursive; font-size:60px">Olympic Information Menu Page</b>
-        <br /><br /><br />
-        <b style="font-family:Gill Sans; font-size:20px;">Hi, visitor! Press one of the following buttons to looking for the Olympic information you wanted!</b> 
-        <br /><br />
-        
-         <form method="GET" action="https://www.students.cs.ubc.ca/~xnie0204/VolunteerPage.php">
-        <p>Learn about the informmation of the Volunteer, please press:&nbsp; 
-            <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Volunteer" name="Volunteer"></p>
-        </form>
-        
-        <form method="GET" action="https://www.students.cs.ubc.ca/~lzy0606/livePlatform.php">
-        <p>Learn about the informmation of the LivePlatform, please press:&nbsp; 
-            <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Live Platform" name="LivePlatform"></p>
-        </form>
+<form method="GET" action="https://www.students.cs.ubc.ca/~lzy0606/livePlatform.php">
+    <p>Learn about the information of the LivePlatform, please press:&nbsp;
+        <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Live Platform" name="LivePlatform"></p>
+</form>
+<form method="GET" action="https://www.students.cs.ubc.ca/~lzy0606/updateMedal.php">
+    <p>Update the athletes' medals, please press:&nbsp;
+        <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Update Medals" name="UpdateMedals"></p>
+</form>
+<p>Get information of Athletes, please press:&nbsp;
+    <a href="https://www.students.cs.ubc.ca/~yzhu33/Athlete.php">
+        <button style="font-family:Gill Sans; font-size:18px;">Athlete</button>
+    </a>
+</p>
+<p>Find Max Amount of Money Support by the Sponsor, please press:&nbsp;
+    <a href="https://www.students.cs.ubc.ca/~yzhu33/Sponsor.php">
+        <button style="font-family:Gill Sans; font-size:18px;">Sponsor</button>
+    </a>
+</p>
 
-        <form method="GET" action="https://www.students.cs.ubc.ca/~lzy0606/updateMedal.php">
-        <p>Update the athletes' medals, please press:&nbsp; 
-            <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Update Medals" name="UpdateMedals"></p>
-        </form>
-
-
-
-        <p>Get information of Athletes, please press:&nbsp;
-            <a href="https://www.students.cs.ubc.ca/~yzhu33/NO456.php">
-                <button style="font-family:Gill Sans; font-size:18px;">Athlete</button>
-            </a>
-        </p>
-
-        <p>Find Max Amount of Money Support by the Sponsor, please press:&nbsp;
-            <a href="https://www.students.cs.ubc.ca/~yzhu33/NO7.php">
-                <button style="font-family:Gill Sans; font-size:18px;">Sponsor</button>
-            </a>
-        </p>
-        
-         <p>Find Extra information , please press:&nbsp;
-            <a href="https://www.students.cs.ubc.ca/~xnie0204/Extra.php">
-                <button style="font-family:Gill Sans; font-size:18px;">Extra</button>
-            </a>
-        </p>
-
-    </body>
-
-
+<p>Find Extra information , please press:&nbsp;
+    <a href="https://www.students.cs.ubc.ca/~xnie0204/Extra.php">
+        <button style="font-family:Gill Sans; font-size:18px;">Extra</button>
+    </a>
+</p>
+</body>
 </html>
