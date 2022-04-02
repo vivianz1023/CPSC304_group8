31c559d6e095ba43f66529ac64adad118f8d8dc7
vivianz1023 <86883079+vivianz1023@users.noreply.github.com>
2022-04-02 11:12:49 -0700

    Update mainPage.php
    add icons

--- mainPage.php
+++ mainPage.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <script src="https://kit.fontawesome.com/f41258b310.js" crossorigin="anonymous"></script>
+
+
     <title>MainPage Information</title>
     <style>
         body {
@@ -18,30 +21,30 @@
 <br /><br />
 
 <form method="GET" action="https://www.students.cs.ubc.ca/~xnie0204/VolunteerPage.php">
-    <p>Learn about the information of the Volunteer, please press:&nbsp;
+    <p><i class="fa-solid fa-handshake-angle" style="font-size:35px;color:lightskyblue" ></i>&nbsp;&nbsp;Learn about the information of the Volunteer, please press:&nbsp;
         <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Volunteer" name="Volunteer"></p>
 </form>
 
 <form method="GET" action="https://www.students.cs.ubc.ca/~lzy0606/livePlatform.php">
-    <p>Learn about the information of the LivePlatform, please press:&nbsp;
+    <p><i class="fa-solid fa-tv" style="font-size:35px;color:moccasin"></i>&nbsp;&nbsp;Learn about the information of the LivePlatform, please press:&nbsp;
         <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Live Platform" name="LivePlatform"></p>
 </form>
 <form method="GET" action="https://www.students.cs.ubc.ca/~lzy0606/updateMedal.php">
-    <p>Update the athletes' medals, please press:&nbsp;
+    <p><i class="fa-solid fa-medal" style="font-size:35px;color:palegreen"></i>&nbsp;&nbsp;Update the athletes' medals, please press:&nbsp;
         <input style="font-family:Gill Sans; font-size:18px;" type="submit" value="Update Medals" name="UpdateMedals"></p>
 </form>
-<p>Get information of Athletes, please press:&nbsp;
+<p><i class="fa-solid fa-person-skating" style="font-size:35px;color:lightpink"></i>&nbsp;&nbsp;Get information of Athletes, please press:&nbsp;
     <a href="https://www.students.cs.ubc.ca/~yzhu33/Athlete.php">
         <button style="font-family:Gill Sans; font-size:18px;">Athlete</button>
     </a>
 </p>
-<p>Find Max Amount of Money Support by the Sponsor, please press:&nbsp;
+<p><i class="fa-solid fa-building" style="font-size:35px;color:cornflowerblue"></i>&nbsp;&nbsp;Find Max Amount of Money Support by the Sponsor, please press:&nbsp;
     <a href="https://www.students.cs.ubc.ca/~yzhu33/Sponsor.php">
         <button style="font-family:Gill Sans; font-size:18px;">Sponsor</button>
     </a>
 </p>
 
-<p>Find Extra information , please press:&nbsp;
+<p><i class="fa-solid fa-circle-info" style="font-size:35px;color:mediumaquamarine"></i>&nbsp;&nbsp;Find Extra information , please press:&nbsp;
     <a href="https://www.students.cs.ubc.ca/~xnie0204/Extra.php">
         <button style="font-family:Gill Sans; font-size:18px;">Extra</button>
     </a>
